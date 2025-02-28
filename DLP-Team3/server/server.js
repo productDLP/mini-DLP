ead7c724aba923ce48555d3178be986b278fd6f0
48-JASHWANTH <ratnajashwanth64@gmail.com>
2025-02-28 20:32:23 +0530

    Backend code skeleton structure

---
+++ DLP-Team3/server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 9643;
+
+app.use(cors());
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+
+app.use('/api', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
